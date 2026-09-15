1d7e41731ebce7d2904ab49a1991d09a8959962d
Ubunteous <>
2026-09-15 17:05:10 +0200

    Remove hunchentoot specific function import

--- src/packages.lisp
+++ src/packages.lisp
@@ -14,14 +14,14 @@
 ;;    #:stop-server))
 
 (uiop:define-package :system
-	(:import-from :hunchentoot
-				  #:easy-acceptor
-				  #:start
-				  #:stop)
-  (:use :cl
-		;; :hunchentoot
-		;; :easy-routes ; conflict with hunchentoot
-		)
+	;; (:import-from :hunchentoot
+	;; 			  #:easy-acceptor
+	;; 			  #:start
+	;; 			  #:stop)
+	(:use :cl
+		  ;; :hunchentoot
+		  ;; :easy-routes ; conflict with hunchentoot
+		  )
   (:export
    #:start-server
    #:stop-server))
